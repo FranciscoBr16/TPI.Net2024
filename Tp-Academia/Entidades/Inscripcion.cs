f153153e53687c467fc627a4bd2dc7a4ad732a42
cufardixx <facundopicia@gmail.com>
2024-08-08 10:07:43 -0300

    agregado de entidades

---
+++ Tp-Academia/Entidades/Inscripcion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Entidades
+{
+    public class Inscripcion
+    {
+        public int idInscripcion { get; set; }
+        public string condicion { get; set; }
+        public string fecha { get; set; }
+        public int nota { get; set; }
+
+        public Alumno alumno { get; set; }
+        public Curso Curso { get; set; }
+
+
+        public Inscripcion(int idInscripcion, string condicion, string fecha, int nota, Alumno alumno, Curso curso)
+        {
+            idInscripcion = idInscripcion;
+            this.condicion = condicion;
+            this.fecha = fecha;
+            this.nota = nota;
+            Alumno = alumno;
+            Curso = curso;
+        }
+    }
+}
+
+
---
+++ Tp-Academia/Entidades/Profesor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Entidades
+{
+    public class Profesor : Persona
+    {
+        public string cargo {  get; set; }
+
+        public Profesor(string cargo)
+        {
+            this.cargo = cargo;
+        }
+    }
+
+}
+
