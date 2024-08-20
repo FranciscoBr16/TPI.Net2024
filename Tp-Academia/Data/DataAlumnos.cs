649c8702e72e2fe19ad5f5bff198ee49e82296f5
FranciscoBBruno <fransbebobruno@gmail.com>
2024-08-20 14:08:04 -0300

    getbylegajo y modificacion en bd

--- Tp-Academia/Data/DataAlumnos.cs
+++ Tp-Academia/Data/DataAlumnos.cs
@@ -4,12 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data
 {
     public class DataAlumnos
     {
-        using(AcademiaContext db = new AcademiaContext()){
+        public DbSet<Alumno> getAlumnos() { 
+        using (AcademiaContext db= new AcademiaContext()){
+
+        var Alumnos = db.Alumnos;
+
+        return Alumnos;
+    }
     }
     }
 }
