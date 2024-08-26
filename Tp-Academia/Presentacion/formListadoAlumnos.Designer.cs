e4f06d25f4792b4f93e2d4341da5f31569129794
FranciscoBr16 <fransbebobruno@gmail.com>
2024-08-26 10:21:22 -0300

    Proyecto win form y tabla alumnos

---
+++ Tp-Academia/Presentacion/formListadoAlumnos.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Presentacion
+{
+    partial class formListadoAlumnos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvAlumnos = new DataGridView();
+            colLegajo = new DataGridViewTextBoxColumn();
+            colNombre = new DataGridViewTextBoxColumn();
+            colApellido = new DataGridViewTextBoxColumn();
+            colUsuario = new DataGridViewTextBoxColumn();
+            colDireccion = new DataGridViewTextBoxColumn();
+            colCorreo = new DataGridViewTextBoxColumn();
+            colBtnModificar = new DataGridViewTextBoxColumn();
+            colBtnEliminar = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvAlumnos).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvAlumnos
+            // 
+            dgvAlumnos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAlumnos.Columns.AddRange(new DataGridViewColumn[] { colLegajo, colNombre, colApellido, colUsuario, colDireccion, colCorreo, colBtnModificar, colBtnEliminar });
+            dgvAlumnos.Dock = DockStyle.Fill;
+            dgvAlumnos.Location = new Point(0, 0);
+            dgvAlumnos.Name = "dgvAlumnos";
+            dgvAlumnos.RowHeadersWidth = 51;
+            dgvAlumnos.Size = new Size(1065, 450);
+            dgvAlumnos.TabIndex = 0;
+            dgvAlumnos.CellContentClick += dgvAlumnos_CellContentClick;
+            // 
+            // colLegajo
+            // 
+            colLegajo.HeaderText = "Legajo";
+            colLegajo.MinimumWidth = 6;
+            colLegajo.Name = "colLegajo";
+            colLegajo.ReadOnly = true;
+            colLegajo.Width = 125;
+            // 
+            // colNombre
+            // 
+            colNombre.HeaderText = "Nombre";
+            colNombre.MinimumWidth = 6;
+            colNombre.Name = "colNombre";
+            colNombre.ReadOnly = true;
+            colNombre.Width = 125;
+            // 
+            // colApellido
+            // 
+            colApellido.HeaderText = "Apellido";
+            colApellido.MinimumWidth = 6;
+            colApellido.Name = "colApellido";
+            colApellido.ReadOnly = true;
+            colApellido.Width = 125;
+            // 
+            // colUsuario
+            // 
+            colUsuario.HeaderText = "Usuario";
+            colUsuario.MinimumWidth = 6;
+            colUsuario.Name = "colUsuario";
+            colUsuario.ReadOnly = true;
+            colUsuario.Width = 125;
+            // 
+            // colDireccion
+            // 
+            colDireccion.HeaderText = "Direccion";
+            colDireccion.MinimumWidth = 6;
+            colDireccion.Name = "colDireccion";
+            colDireccion.ReadOnly = true;
+            colDireccion.Width = 125;
+            // 
+            // colCorreo
+            // 
+            colCorreo.HeaderText = "Correo";
+            colCorreo.MinimumWidth = 6;
+            colCorreo.Name = "colCorreo";
+            colCorreo.Width = 125;
+            // 
+            // colBtnModificar
+            // 
+            colBtnModificar.HeaderText = "";
+            colBtnModificar.MinimumWidth = 6;
+            colBtnModificar.Name = "colBtnModificar";
+            colBtnModificar.ReadOnly = true;
+            colBtnModificar.Width = 125;
+            // 
+            // colBtnEliminar
+            // 
+            colBtnEliminar.HeaderText = "";
+            colBtnEliminar.MinimumWidth = 6;
+            colBtnEliminar.Name = "colBtnEliminar";
+            colBtnEliminar.ReadOnly = true;
+            colBtnEliminar.Width = 125;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1065, 450);
+            Controls.Add(dgvAlumnos);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvAlumnos).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvAlumnos;
+        private DataGridViewTextBoxColumn colLegajo;
+        private DataGridViewTextBoxColumn colNombre;
+        private DataGridViewTextBoxColumn colApellido;
+        private DataGridViewTextBoxColumn colUsuario;
+        private DataGridViewTextBoxColumn colDireccion;
+        private DataGridViewTextBoxColumn colCorreo;
+        private DataGridViewTextBoxColumn colBtnModificar;
+        private DataGridViewTextBoxColumn colBtnEliminar;
+    }
+}
---
+++ Tp-Academia/Presentacion/formListadoAlumnos.cs
@@ -0,0 +1,42 @@
+﻿using Data;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class formListadoAlumnos : Form
+    {
+        public formListadoAlumnos()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvAlumnos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        public void Listar()
+        {
+           
+            DataAlumnos dataAlumnos = new DataAlumnos();
+
+            List<Alumno> listaAlumnos = dataAlumnos.getAlumnos().ToList();
+            
+            // Todavia no se que hacer aca
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+    }
+}
