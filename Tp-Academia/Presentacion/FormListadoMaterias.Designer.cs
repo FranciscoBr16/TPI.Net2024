b5cfb1ecfbb7005020a97c5524e83a040f5e37aa
FranciscoBBruno <fransbebobruno@gmail.com>
2024-09-26 10:38:35 -0300

    agregado de listado de materias - esta sin terminar

---
+++ Tp-Academia/Presentacion/FormListadoMaterias.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Presentacion
+{
+    partial class FormListadoMaterias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            dgvMaterias = new DataGridView();
+            tableLayoutPanel5 = new TableLayoutPanel();
+            lblMaterias = new Label();
+            btnNuevaMateria = new Button();
+            Id_materia = new DataGridViewTextBoxColumn();
+            Horas_semanales = new DataGridViewTextBoxColumn();
+            Nombre = new DataGridViewTextBoxColumn();
+            Descripcion = new DataGridViewTextBoxColumn();
+            Id_Plan = new DataGridViewTextBoxColumn();
+            colBtnModificarCom = new DataGridViewButtonColumn();
+            colBtnEliminarCom = new DataGridViewButtonColumn();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvMaterias).BeginInit();
+            tableLayoutPanel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 90F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
+            tableLayoutPanel1.Controls.Add(dgvMaterias, 1, 0);
+            tableLayoutPanel1.Dock = DockStyle.Top;
+            tableLayoutPanel1.Location = new Point(0, 107);
+            tableLayoutPanel1.Margin = new Padding(3, 2, 3, 2);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(996, 340);
+            tableLayoutPanel1.TabIndex = 9;
+            // 
+            // dgvMaterias
+            // 
+            dgvMaterias.Anchor = AnchorStyles.Top;
+            dgvMaterias.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvMaterias.Columns.AddRange(new DataGridViewColumn[] { Id_materia, Horas_semanales, Nombre, Descripcion, Id_Plan, colBtnModificarCom, colBtnEliminarCom });
+            dgvMaterias.Location = new Point(79, 2);
+            dgvMaterias.Margin = new Padding(3, 2, 3, 2);
+            dgvMaterias.Name = "dgvMaterias";
+            dgvMaterias.RowHeadersWidth = 51;
+            dgvMaterias.Size = new Size(836, 306);
+            dgvMaterias.TabIndex = 8;
+            // 
+            // tableLayoutPanel5
+            // 
+            tableLayoutPanel5.ColumnCount = 3;
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel5.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel5.Controls.Add(lblMaterias, 1, 0);
+            tableLayoutPanel5.Controls.Add(btnNuevaMateria, 1, 1);
+            tableLayoutPanel5.Dock = DockStyle.Top;
+            tableLayoutPanel5.Location = new Point(0, 0);
+            tableLayoutPanel5.Name = "tableLayoutPanel5";
+            tableLayoutPanel5.RowCount = 2;
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel5.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel5.Size = new Size(996, 107);
+            tableLayoutPanel5.TabIndex = 8;
+            // 
+            // lblMaterias
+            // 
+            lblMaterias.Anchor = AnchorStyles.None;
+            lblMaterias.AutoSize = true;
+            lblMaterias.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblMaterias.Location = new Point(455, 14);
+            lblMaterias.Name = "lblMaterias";
+            lblMaterias.Size = new Size(85, 25);
+            lblMaterias.TabIndex = 0;
+            lblMaterias.Text = "Materias";
+            // 
+            // btnNuevaMateria
+            // 
+            btnNuevaMateria.Anchor = AnchorStyles.Bottom;
+            btnNuevaMateria.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnNuevaMateria.Location = new Point(418, 77);
+            btnNuevaMateria.Name = "btnNuevaMateria";
+            btnNuevaMateria.Size = new Size(160, 27);
+            btnNuevaMateria.TabIndex = 1;
+            btnNuevaMateria.Text = "Agregar Materia";
+            btnNuevaMateria.UseVisualStyleBackColor = true;
+            // 
+            // Id_materia
+            // 
+            Id_materia.DataPropertyName = "Id";
+            Id_materia.HeaderText = "Id";
+            Id_materia.Name = "Id_materia";
+            Id_materia.ReadOnly = true;
+            // 
+            // Horas_semanales
+            // 
+            Horas_semanales.DataPropertyName = "CantHorasSemanales";
+            Horas_semanales.HeaderText = "Horas semanales";
+            Horas_semanales.Name = "Horas_semanales";
+            Horas_semanales.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            Nombre.DataPropertyName = "Nombre";
+            Nombre.HeaderText = "Nombre";
+            Nombre.Name = "Nombre";
+            Nombre.ReadOnly = true;
+            // 
+            // Descripcion
+            // 
+            Descripcion.DataPropertyName = "Descripcion";
+            Descripcion.HeaderText = "Descripcion";
+            Descripcion.Name = "Descripcion";
+            Descripcion.ReadOnly = true;
+            // 
+            // Id_Plan
+            // 
+            Id_Plan.DataPropertyName = "PlanId";
+            Id_Plan.HeaderText = "Id Plan";
+            Id_Plan.Name = "Id_Plan";
+            Id_Plan.ReadOnly = true;
+            // 
+            // colBtnModificarCom
+            // 
+            colBtnModificarCom.HeaderText = "";
+            colBtnModificarCom.MinimumWidth = 6;
+            colBtnModificarCom.Name = "colBtnModificarCom";
+            colBtnModificarCom.ReadOnly = true;
+            colBtnModificarCom.Text = "Modificar";
+            colBtnModificarCom.UseColumnTextForButtonValue = true;
+            colBtnModificarCom.Width = 125;
+            // 
+            // colBtnEliminarCom
+            // 
+            colBtnEliminarCom.HeaderText = "";
+            colBtnEliminarCom.MinimumWidth = 6;
+            colBtnEliminarCom.Name = "colBtnEliminarCom";
+            colBtnEliminarCom.ReadOnly = true;
+            colBtnEliminarCom.Text = "Eliminar";
+            colBtnEliminarCom.UseColumnTextForButtonValue = true;
+            colBtnEliminarCom.Width = 125;
+            // 
+            // FormListadoMaterias
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(996, 590);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(tableLayoutPanel5);
+            Name = "FormListadoMaterias";
+            Text = "FormListadoMaterias";
+            WindowState = FormWindowState.Maximized;
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvMaterias).EndInit();
+            tableLayoutPanel5.ResumeLayout(false);
+            tableLayoutPanel5.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private DataGridView dgvMaterias;
+        private TableLayoutPanel tableLayoutPanel5;
+        private Label lblMaterias;
+        private Button btnNuevaMateria;
+        private DataGridViewTextBoxColumn Id_materia;
+        private DataGridViewTextBoxColumn Horas_semanales;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Descripcion;
+        private DataGridViewTextBoxColumn Id_Plan;
+        private DataGridViewButtonColumn colBtnModificarCom;
+        private DataGridViewButtonColumn colBtnEliminarCom;
+    }
+}
--- Tp-Academia/Presentacion/FormLogIn.Designer.cs
+++ Tp-Academia/Presentacion/FormLogIn.Designer.cs
@@ -33,11 +33,11 @@
             tableLayoutPanel2 = new TableLayoutPanel();
             lblUsuario = new Label();
             lblClave = new Label();
+            tbUsuario = new TextBox();
+            tbClave = new TextBox();
             tableLayoutPanel3 = new TableLayoutPanel();
             btnCancelar = new Button();
             btnAceptar = new Button();
-            tbUsuario = new TextBox();
-            tbClave = new TextBox();
             tableLayoutPanel1.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
             tableLayoutPanel3.SuspendLayout();
@@ -52,10 +52,11 @@
             tableLayoutPanel1.Controls.Add(label1, 1, 0);
             tableLayoutPanel1.Dock = DockStyle.Top;
             tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Margin = new Padding(3, 2, 3, 2);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(819, 84);
+            tableLayoutPanel1.Size = new Size(717, 63);
             tableLayoutPanel1.TabIndex = 1;
             // 
             // label1
@@ -63,9 +64,9 @@
             label1.Anchor = AnchorStyles.None;
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(300, 19);
+            label1.Location = new Point(272, 13);
             label1.Name = "label1";
-            label1.Size = new Size(215, 46);
+            label1.Size = new Size(172, 37);
             label1.TabIndex = 0;
             label1.Text = "Iniciar Sesión";
             // 
@@ -81,22 +82,23 @@
             tableLayoutPanel2.Controls.Add(tbUsuario, 2, 1);
             tableLayoutPanel2.Controls.Add(tbClave, 2, 2);
             tableLayoutPanel2.Dock = DockStyle.Top;
-            tableLayoutPanel2.Location = new Point(0, 84);
+            tableLayoutPanel2.Location = new Point(0, 63);
+            tableLayoutPanel2.Margin = new Padding(3, 2, 3, 2);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 3;
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 15F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel2.Size = new Size(819, 199);
+            tableLayoutPanel2.Size = new Size(717, 168);
             tableLayoutPanel2.TabIndex = 2;
             // 
             // lblUsuario
             // 
             lblUsuario.Anchor = AnchorStyles.None;
             lblUsuario.AutoSize = true;
-            lblUsuario.Location = new Point(115, 54);
+            lblUsuario.Location = new Point(103, 45);
             lblUsuario.Name = "lblUsuario";
-            lblUsuario.Size = new Size(54, 20);
+            lblUsuario.Size = new Size(42, 15);
             lblUsuario.TabIndex = 0;
             lblUsuario.Text = "Legajo";
             // 
@@ -104,11 +106,30 @@
             // 
             lblClave.Anchor = AnchorStyles.None;
             lblClave.AutoSize = true;
-            lblClave.Location = new Point(100, 144);
+            lblClave.Location = new Point(91, 122);
             lblClave.Name = "lblClave";
-            lblClave.Size = new Size(83, 20);
+            lblClave.Size = new Size(67, 15);
             lblClave.TabIndex = 1;
             lblClave.Text = "Contraseña";
+            // 
+            // tbUsuario
+            // 
+            tbUsuario.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            tbUsuario.Location = new Point(181, 41);
+            tbUsuario.Margin = new Padding(3, 2, 3, 2);
+            tbUsuario.Name = "tbUsuario";
+            tbUsuario.Size = new Size(460, 23);
+            tbUsuario.TabIndex = 2;
+            // 
+            // tbClave
+            // 
+            tbClave.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            tbClave.Location = new Point(181, 118);
+            tbClave.Margin = new Padding(3, 2, 3, 2);
+            tbClave.Name = "tbClave";
+            tbClave.PasswordChar = '*';
+            tbClave.Size = new Size(460, 23);
+            tbClave.TabIndex = 3;
             // 
             // tableLayoutPanel3
             // 
@@ -120,20 +141,22 @@
             tableLayoutPanel3.Controls.Add(btnCancelar, 1, 0);
             tableLayoutPanel3.Controls.Add(btnAceptar, 2, 0);
             tableLayoutPanel3.Dock = DockStyle.Top;
-            tableLayoutPanel3.Location = new Point(0, 283);
+            tableLayoutPanel3.Location = new Point(0, 231);
+            tableLayoutPanel3.Margin = new Padding(3, 2, 3, 2);
             tableLayoutPanel3.Name = "tableLayoutPanel3";
             tableLayoutPanel3.RowCount = 1;
             tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel3.Size = new Size(819, 63);
+            tableLayoutPanel3.Size = new Size(717, 47);
             tableLayoutPanel3.TabIndex = 48;
             // 
             // btnCancelar
             // 
             btnCancelar.Anchor = AnchorStyles.None;
             btnCancelar.BackColor = Color.Red;
-            btnCancelar.Location = new Point(259, 17);
+            btnCancelar.Location = new Point(227, 12);
+            btnCancelar.Margin = new Padding(3, 2, 3, 2);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(94, 29);
+            btnCancelar.Size = new Size(82, 22);
             btnCancelar.TabIndex = 20;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = false;
@@ -141,40 +164,26 @@
             // btnAceptar
             // 
             btnAceptar.Anchor = AnchorStyles.None;
-            btnAceptar.Location = new Point(463, 17);
+            btnAceptar.Location = new Point(406, 12);
+            btnAceptar.Margin = new Padding(3, 2, 3, 2);
             btnAceptar.Name = "btnAceptar";
-            btnAceptar.Size = new Size(94, 29);
+            btnAceptar.Size = new Size(82, 22);
             btnAceptar.TabIndex = 19;
             btnAceptar.Text = "Aceptar";
             btnAceptar.UseVisualStyleBackColor = true;
             btnAceptar.Click += btnAceptar_Click;
             // 
-            // tbUsuario
-            // 
-            tbUsuario.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-            tbUsuario.Location = new Point(206, 51);
-            tbUsuario.Name = "tbUsuario";
-            tbUsuario.Size = new Size(526, 27);
-            tbUsuario.TabIndex = 2;
-            // 
-            // tbClave
-            // 
-            tbClave.Anchor = AnchorStyles.Left | AnchorStyles.Right;
-            tbClave.Location = new Point(206, 140);
-            tbClave.Name = "tbClave";
-            tbClave.PasswordChar = '*';
-            tbClave.Size = new Size(526, 27);
-            tbClave.TabIndex = 3;
-            // 
             // FormLogIn
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(819, 376);
+            ClientSize = new Size(717, 304);
             Controls.Add(tableLayoutPanel3);
             Controls.Add(tableLayoutPanel2);
             Controls.Add(tableLayoutPanel1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "FormLogIn";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Log In";
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
